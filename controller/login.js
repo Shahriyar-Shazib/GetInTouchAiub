f3ced23f139fd8dee1d80051619f75df2a8b193a
Al-sany Samil <alsanysamil@gmail.com>
2020-11-17 23:25:57 +0600

    content controller route added

--- controller/login.js
+++ controller/login.js
@@ -20,10 +20,11 @@ router.post('/', (req, res)=>{
 				req.session.type = result[0].usertype;
 				res.cookie('uname', req.body.username);
 				res.redirect('/Adminhome');
-				//console.log(result[0].accountstatus);
-			}
-			//res.cookie('uname', req.body.username);
-			//res.redirect('/home');	
+			}	
+		}else if((result[0].accountstatus == "Active") && (result[0].usertype == "Content Control Manager")){
+			req.session.type = result[0].usertype;
+			res.cookie('uname', req.body.username);
+			res.redirect('/contentcontroller');
 		}else{
 			res.redirect('/login');
 		}
