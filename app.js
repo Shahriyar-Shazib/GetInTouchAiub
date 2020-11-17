365d2b959f6ffb73dd7845507f9ec6d32e1f215d
Al-sany Samil <alsanysamil@gmail.com>
2020-11-17 23:10:08 +0600

    content controller route added

--- app.js
+++ app.js
@@ -27,11 +27,9 @@ app.use('/logout', logout);
 app.get('/', (req, res)=>{
 	if(req.cookies['uname'] != null && req.session.type=="Admin"){
 		res.redirect('/Adminhome');
-	}else if(req.cookies['uname'] != null && req.session.type=="Admin"){
-		res.redirect('/ccmhome');
 	}else{
 		res.redirect('/login');
-	}		
+	}	
 });
 
 //server startup
