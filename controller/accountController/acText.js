11655916963b716e4cbd7a7e4df346a6cb4e0764
NayeemHasan807 <moonnayeem87@gmail.com>
2020-11-24 01:32:23 +0600

    Send Text Validation

--- controller/accountController/acText.js
+++ controller/accountController/acText.js
@@ -1,9 +1,11 @@
-const express 	= require('express');
-const acAdminModel = require.main.require('./models/accountControlManeger/acAdminModel');
-const acCCModel = require.main.require('./models/accountControlManeger/acCCModel');
-const acGUModel = require.main.require('./models/accountControlManeger/acGUModel');
-const acTextModel = require.main.require('./models/accountControlManeger/acTextModel');
-const router 	= express.Router();
+const express 		= require('express');
+const acAdminModel 	= require.main.require('./models/accountControlManeger/acAdminModel');
+const acCCModel 	= require.main.require('./models/accountControlManeger/acCCModel');
+const acGUModel		= require.main.require('./models/accountControlManeger/acGUModel');
+const acTextModel	= require.main.require('./models/accountControlManeger/acTextModel');
+const router 		= express.Router();
+const bodyParser 	= require('body-parser');
+const{ check , validationResult } = require('express-validator');
 
 router.get('/CreateText', (req, res)=>{
 	if(req.cookies['uname'] != null && req.cookies['usertype'] == "Account Control Manager"){
@@ -14,25 +16,50 @@ router.get('/CreateText', (req, res)=>{
 	}
 })
 
-router.post('/CreateText', (req, res)=>{
-	let data = {
-		acid : req.body.acid,
-		text : req.body.text,
-		receiverid : req.body.receiverid
-	};
-	
-	if(req.cookies['uname'] != null && req.cookies['usertype'] == "Account Control Manager"){
+router.post('/CreateText' , [
+		check('receiverid','Can not be empty')
+			.notEmpty()
+		,
+		check('text','Can not be empty')
+			.notEmpty()
+	] , (req, res)=>{
+
+	const errors = validationResult(req);
+	if(errors.isEmpty())
+	{
+		let data = {
+			acid : req.body.acid,
+			text : req.body.text,
+			receiverid : req.body.receiverid
+		};
 		
-		acTextModel.createText(data, function(status){
-		if(status){
-			 res.status(200).send({ status : 'Text Send Successfully!!' });
+		if(req.cookies['uname'] != null && req.cookies['usertype'] == "Account Control Manager"){
+			
+			acTextModel.createText(data, function(status){
+			if(status){
+				 res.status(200).send({ status : 'Text Send Successfully!!' });
+			}else{
+				 res.status(200).send({ status : 'Fail to send Text!!'});
+			}
+		});
 		}else{
-			 res.status(200).send({ status : 'Fail to send Text!!'});
+			res.redirect('/login');
 		}
-	});
-	}else{
-		res.redirect('/login');
 	}
+	else
+	{
+		console.log(errors.array());
+		var em = errors.array();
+		var errormassage = ``;
+
+		for(i=0 ; i<em.length ; i++)
+		{
+			errormassage=errormassage+ em[i].param + " : " + em[i].msg +"<br/>"
+		}
+
+		res.status(200).send({ status : errormassage });
+	}
+	
 })
 
 router.get('/Text', (req, res)=>{
