05c1864fa0fb5bf5262f3c7cb9e4320b4f9d4f48
NayeemHasan807 <moonnayeem87@gmail.com>
2020-11-25 05:22:14 +0600

    Little update on ac home

--- controller/accountController/acHome.js
+++ controller/accountController/acHome.js
@@ -1,10 +1,15 @@
 const express 	= require('express');
+const acModel = require.main.require('./models/accountControlManeger/acModel');
 const router 	= express.Router();
 
 router.get('/', (req, res)=>{
-	
 	if(req.cookies['uname'] != null && req.cookies['usertype'] == "Account Control Manager"){
-		res.render('accountControlManager/acHome', {uname : req.cookies['uname']});
+		var data={
+			acid : req.cookies['uname']  
+		};
+		acModel.getMyInfo(data , function(results){
+			res.render('accountControlManager/acHome', {user: results});
+		});
 	}else{
 		res.redirect('/login');
 	}
